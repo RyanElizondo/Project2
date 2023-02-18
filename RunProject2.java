d56530d09f2e447d0dd51f260a5148ef880e7fe4
Ryan Elizondo-Fallas <ryanelizondo10@gmail.com>
2023-02-17 21:43:06 -0500

    Add files via upload

---
+++ RunProject2.java
@@ -0,0 +1,9 @@
+/**
+ * This class is the driver class for project 2.
+ * @author Ryan Elizondo-Fallas, Alex Virilli
+ */
+public class RunProject2 {
+    public static void main(String[] args) {
+        new tutitionmanager().run();
+    }
+}
